3e775b7e0f57d1c9563b1f9bad7f7085c653a947
Hariadi Hinta <diperakui@yahoo.com>
2013-05-09 23:39:23 +0800

    Initial structure

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*
+ * malay-name-parser
+ * http://github.com/hariadi/malay-name-parser
+ *
+ * Copyright (c) 2013 Hariadi Hinta
+ * MIT License
+ */
+
+module.exports = function(grunt) {
+
+'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Configuration to be run (and then tested).
+    coffee: {
+      parser: {
+        expand: true,
+        cwd: 'src/lib',
+        src: [
+          '*.coffee'
+        ],
+        dest: 'lib/',
+        ext: '.js'
+      },
+      tests: {
+        expand: true,
+        cwd: 'src/test',
+        src: ['**/*.coffee'],
+        dest: 'test/',
+        ext: '.js'
+      }
+    },
+
+    // Run mocha tests.
+    mochaTest: {
+      files: ['test/*_test.js']
+    },
+    mochaTestConfig: {
+      options: {
+        reporter: 'spec'
+      }
+    }
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('default', [
+    'coffee'
+  ]);
+  // Build templates using parser and run all tests.
+  grunt.registerTask('test', [
+    'coffee',
+    'mochaTest'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,35 @@
+# Malay Name Parser
+
+> A simple script for parsing complex Malay names into their individual components.
+
+Parses Malay names into 5 components:
+- Title
+- First name
+- Middle names
+- Last names
+- Suffixes
+
+
+## Quick start
+For linting and testing this project uses Grunt `~0.4.1`, but Grunt is **not required** to use this parser. Check out the [Getting Started](http://gruntjs.com/getting-started) guide to learn more about Grunt.
+
+`npm install malay-name-parser`
+
+An example:
+```js
+parser = require('malay-name-parser')
+name = parser.parse_malay_name('Dato\' Ir. Dr. Hariadi Hinta')
+console.log(name.title, name.first, name.suffixes, name.middle_names, name.last_names)
+```
+
+
+## Credit
+> Many of these name parser logic come from the following repos:
+
+* [Javascript version, by John Griffey](https://github.com/redjohn/name_parser)
+* [Python version, by Derek Gulbranson](http://code.google.com/p/python-nameparser)
+* [PHP version, by G. Miernicki](http://code.google.com/p/nameparser/)
+
+
+## Release History
+* 2013-05-09	v0.1.0			First commit
